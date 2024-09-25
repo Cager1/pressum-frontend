1ddcf94462baee8d36955f9ea6d5d0fa3c6532ec
DaniiM <74544641+DaniiM@users.noreply.github.com>
2024-09-25 13:11:20 +0200

    knjige/e-knjige dodano na plavu alatnu traku

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,58 @@
 <template>
   <v-app>
-    <v-navigation-drawer disable-resize-watcher app v-model="navDrawer" class="d-sm-none">
+    <v-navigation-drawer
+      disable-resize-watcher
+      app
+      v-model="navDrawer"
+      class="d-sm-none"
+    >
       <v-row style="background-color: #084776">
         <v-col class="text-center mt-3">
-          <img :src="require('../assets/images/pressum-logo-white.svg')"
-               alt="logo"
-               height="50"
-          >
+          <img
+            :src="require('../assets/images/pressum-logo-white.svg')"
+            alt="logo"
+            height="50"
+          />
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <nuxt-link class="d-block mt-4" to="/" style="text-decoration: none !important;">
+          <nuxt-link
+            class="d-block mt-4"
+            to="/"
+            style="text-decoration: none !important"
+          >
+            <v-btn tile text color="grey darken-2" width="100%" x-large
+              ><v-icon class="mr-10">mdi-home</v-icon>Početna<v-spacer
+              ></v-spacer
+            ></v-btn>
+          </nuxt-link>
+
+          <nuxt-link
+            v-if="user.role !== undefined"
+            class="d-block mt-4"
+            to="/admin/knjige"
+            style="text-decoration: none !important"
+          >
             <v-btn
+              v-if="
+                user.role.name === 'Super Admin' || user.role.name === 'Admin'
+              "
               tile
               text
               color="grey darken-2"
               width="100%"
               x-large
-
-            ><v-icon class="mr-10">mdi-home</v-icon>Početna<v-spacer></v-spacer></v-btn>
+              ><v-icon class="mr-10">mdi-shield-account</v-icon>Admin<v-spacer
+              ></v-spacer
+            ></v-btn>
           </nuxt-link>
-
-          <nuxt-link v-if="user.role !== undefined" class="d-block mt-4" to="/admin/knjige" style="text-decoration: none !important;">
-            <v-btn
-              v-if="user.role.name === 'Super Admin' || user.role.name === 'Admin'"
-              tile
-              text
-              color="grey darken-2"
-              width="100%"
-              x-large
-
-            ><v-icon class="mr-10">mdi-shield-account</v-icon>Admin<v-spacer></v-spacer></v-btn>
-          </nuxt-link>
-          <a v-if="logged" class="d-block mt-4" :href="`${oauthUrl}/logout?redirect_to=${appUrl}`" style="text-decoration: none !important;">
+          <a
+            v-if="logged"
+            class="d-block mt-4"
+            :href="`${oauthUrl}/logout?redirect_to=${appUrl}`"
+            style="text-decoration: none !important"
+          >
             <v-btn
               tile
               text
@@ -41,67 +60,75 @@
               width="100%"
               x-large
               @click="logout"
-
-            ><v-icon class="mr-10">mdi-logout</v-icon>Odjava<v-spacer></v-spacer></v-btn>
+              ><v-icon class="mr-10">mdi-logout</v-icon>Odjava<v-spacer
+              ></v-spacer
+            ></v-btn>
           </a>
-          <a v-else class="d-block mt-4" :href="`${oauthUrl}/login?redirect_to=${appUrl}`" style="text-decoration: none !important;">
-            <v-btn
-              tile
-              text
-              color="grey darken-2"
-              width="100%"
-              x-large
-
-            ><v-icon class="mr-10">mdi-login</v-icon>Prijava<v-spacer></v-spacer></v-btn>
+          <a
+            v-else
+            class="d-block mt-4"
+            :href="`${oauthUrl}/login?redirect_to=${appUrl}`"
+            style="text-decoration: none !important"
+          >
+            <v-btn tile text color="grey darken-2" width="100%" x-large
+              ><v-icon class="mr-10">mdi-login</v-icon>Prijava<v-spacer
+              ></v-spacer
+            ></v-btn>
           </a>
-
         </v-col>
       </v-row>
-
-
     </v-navigation-drawer>
     <v-app-bar style="background-color: #084776" app>
       <nuxt-link to="/">
-        <img :src="require('../assets/images/pressum-logo-white.svg')"
-             alt="logo"
-             height="50"
-        >
+        <img
+          :src="require('../assets/images/pressum-logo-white.svg')"
+          alt="logo"
+          height="50"
+        />
+      </nuxt-link>
+      <nuxt-link to="/" style="height: 50%">
+        <p>Knjige/e-Knjige</p>
       </nuxt-link>
       <v-spacer></v-spacer>
-      <nuxt-link class="d-none d-sm-flex"  to="/" style="text-decoration: none !important; height: 100%">
-        <v-btn
-          text
-          color="white"
-          height="100%"
-          tile
-        >Početna</v-btn>
+      <nuxt-link
+        class="d-none d-sm-flex"
+        to="/"
+        style="text-decoration: none !important; height: 100%"
+      >
+        <v-btn text color="white" height="100%" tile>Početna</v-btn>
       </nuxt-link>
-      <nuxt-link v-if="user.role !== undefined" class="d-none d-sm-flex"  to="/admin/knjige" style="text-decoration: none !important; height: 100%">
+      <nuxt-link
+        v-if="user.role !== undefined"
+        class="d-none d-sm-flex"
+        to="/admin/knjige"
+        style="text-decoration: none !important; height: 100%"
+      >
         <v-btn
           v-if="user.role.name === 'Super Admin' || user.role.name === 'Admin'"
           text
           color="white"
           height="100%"
           tile
-        >Admin</v-btn>
+          >Admin</v-btn
+        >
       </nuxt-link>
-      <a v-if="logged" class="d-none d-sm-flex" :href="`${oauthUrl}/logout?redirect_to=${appUrl}`" style="text-decoration: none !important; height: 100%">
-      <v-btn
-        tile
-        text
-        color="white"
-        height="100%"
-        @click="logout"
-      >Odjava</v-btn>
+      <a
+        v-if="logged"
+        class="d-none d-sm-flex"
+        :href="`${oauthUrl}/logout?redirect_to=${appUrl}`"
+        style="text-decoration: none !important; height: 100%"
+      >
+        <v-btn tile text color="white" height="100%" @click="logout"
+          >Odjava</v-btn
+        >
       </a>
-      <a v-else  class="d-none d-sm-flex" :href="`${oauthUrl}/login?redirect_to=${appUrl}`"  style="text-decoration: none !important; height: 100%">
-        <v-btn
-          tile
-          text
-          color="white"
-          height="100%"
-
-        >Prijava</v-btn>
+      <a
+        v-else
+        class="d-none d-sm-flex"
+        :href="`${oauthUrl}/login?redirect_to=${appUrl}`"
+        style="text-decoration: none !important; height: 100%"
+      >
+        <v-btn tile text color="white" height="100%">Prijava</v-btn>
       </a>
       <v-btn
         plain
@@ -112,39 +139,50 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-main v-if="!$slots.default" style="min-height: 500px !important;" class="mb-16">
+    <v-main
+      v-if="!$slots.default"
+      style="min-height: 500px !important"
+      class="mb-16 mt-0"
+    >
       <nuxt v-if="!loading" />
       <v-overlay :value="loading">
-        <v-progress-circular indeterminate size="128" width="10"></v-progress-circular>
+        <v-progress-circular
+          indeterminate
+          size="128"
+          width="10"
+        ></v-progress-circular>
       </v-overlay>
     </v-main>
-    <v-main v-else style="min-height: 500px !important;" class="mb-16">
+    <v-main v-else style="min-height: 500px !important" class="mb-16">
       <slot v-if="!loading" />
       <v-overlay :value="loading">
-        <v-progress-circular indeterminate size="128" width="10"></v-progress-circular>
+        <v-progress-circular
+          indeterminate
+          size="128"
+          width="10"
+        ></v-progress-circular>
       </v-overlay>
     </v-main>
     <Footer />
     <application-snackbar />
   </v-app>
-
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
+  name: "DefaultLayout",
 
-  data () {
+  data() {
     return {
       navDrawer: false,
       logged: false,
-    }
+    };
   },
 
   mounted() {
     this.$store.dispatch("userStore/getUser");
     this.loggedIn();
-    console.log(this.$store.getters['userStore/userGetter'])
+    console.log(this.$store.getters["userStore/userGetter"]);
   },
   computed: {
     user() {
@@ -159,7 +197,7 @@ export default {
     },
     appUrl() {
       return process.env.NUXT_APP_URL;
-    }
+    },
   },
 
   methods: {
@@ -168,15 +206,21 @@ export default {
     },
 
     async loggedIn() {
-      this.logged = Object.keys(this.$store.getters['userStore/userGetter']).length > 0;
+      this.logged =
+        Object.keys(this.$store.getters["userStore/userGetter"]).length > 0;
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
- nuxt-link {
-   text-decoration: none !important;
-   color: white !important;
- }
+nuxt-link {
+  text-decoration: none !important;
+  color: white !important;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
 </style>
